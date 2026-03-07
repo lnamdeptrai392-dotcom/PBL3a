28cdbb64998823467234209a34db11bae35d87b7
Phạm Thị Ngọc Khuê <ptnkhuee@gmail.com>
2026-03-07 23:31:58 +0700

    upload UI of AdminTC

---
+++ AdminTC/LuongGV.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace PBL3a.UI.AdminTC
+{
+    partial class LuongGV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            LaL = new Label();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            tbTL = new TextBox();
+            laMGV = new Label();
+            laTGV = new Label();
+            tbML = new TextBox();
+            listView1 = new ListView();
+            btT = new Button();
+            btCheckL = new Button();
+            btSetL = new Button();
+            tableLayoutPanel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // LaL
+            // 
+            LaL.AutoSize = true;
+            tableLayoutPanel1.SetColumnSpan(LaL, 6);
+            LaL.Dock = DockStyle.Fill;
+            LaL.Font = new Font("Segoe UI", 20.1428585F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LaL.Location = new Point(23, 0);
+            LaL.Name = "LaL";
+            LaL.Size = new Size(1501, 132);
+            LaL.TabIndex = 0;
+            LaL.Text = "QUẢN LÝ LƯƠNG GIẢNG VIÊN";
+            LaL.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 8;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 33.02892F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 66.97108F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 274F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 395F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 40F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 255F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Controls.Add(tbTL, 4, 1);
+            tableLayoutPanel1.Controls.Add(LaL, 1, 0);
+            tableLayoutPanel1.Controls.Add(laMGV, 1, 1);
+            tableLayoutPanel1.Controls.Add(laTGV, 3, 1);
+            tableLayoutPanel1.Controls.Add(tbML, 2, 1);
+            tableLayoutPanel1.Controls.Add(listView1, 1, 3);
+            tableLayoutPanel1.Controls.Add(btT, 6, 6);
+            tableLayoutPanel1.Controls.Add(btCheckL, 6, 5);
+            tableLayoutPanel1.Controls.Add(btSetL, 6, 4);
+            tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Location = new Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 69.3333359F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 30.666666F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 37F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 226F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 143F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 106F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 98F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.Size = new Size(1548, 821);
+            tableLayoutPanel1.TabIndex = 1;
+            // 
+            // tbTL
+            // 
+            tableLayoutPanel1.SetColumnSpan(tbTL, 2);
+            tbTL.Dock = DockStyle.Fill;
+            tbTL.Enabled = false;
+            tbTL.Location = new Point(840, 135);
+            tbTL.Name = "tbTL";
+            tbTL.Size = new Size(429, 35);
+            tbTL.TabIndex = 4;
+            // 
+            // laMGV
+            // 
+            laMGV.AutoSize = true;
+            laMGV.Dock = DockStyle.Right;
+            laMGV.Font = new Font("Segoe UI", 12F);
+            laMGV.Location = new Point(52, 132);
+            laMGV.Name = "laMGV";
+            laMGV.Size = new Size(144, 58);
+            laMGV.TabIndex = 1;
+            laMGV.Text = "Mã Giảng Viên: ";
+            // 
+            // laTGV
+            // 
+            laTGV.AutoSize = true;
+            laTGV.Dock = DockStyle.Right;
+            laTGV.Font = new Font("Segoe UI", 12F);
+            laTGV.Location = new Point(617, 132);
+            laTGV.Name = "laTGV";
+            laTGV.Size = new Size(217, 58);
+            laTGV.TabIndex = 2;
+            laTGV.Text = "Tên Giảng Viên: ";
+            // 
+            // tbML
+            // 
+            tbML.Dock = DockStyle.Fill;
+            tbML.Location = new Point(202, 135);
+            tbML.Name = "tbML";
+            tbML.Size = new Size(358, 35);
+            tbML.TabIndex = 3;
+            // 
+            // listView1
+            // 
+            tableLayoutPanel1.SetColumnSpan(listView1, 4);
+            listView1.Dock = DockStyle.Fill;
+            listView1.Location = new Point(23, 230);
+            listView1.Name = "listView1";
+            tableLayoutPanel1.SetRowSpan(listView1, 4);
+            listView1.Size = new Size(1206, 567);
+            listView1.TabIndex = 8;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // btT
+            // 
+            btT.Dock = DockStyle.Bottom;
+            btT.Font = new Font("Segoe UI", 9.857143F);
+            btT.Location = new Point(1275, 721);
+            btT.Name = "btT";
+            btT.Size = new Size(249, 76);
+            btT.TabIndex = 7;
+            btT.Text = "Thoát";
+            btT.UseVisualStyleBackColor = true;
+            // 
+            // btCheckL
+            // 
+            btCheckL.Dock = DockStyle.Bottom;
+            btCheckL.Font = new Font("Segoe UI", 9.857143F);
+            btCheckL.Location = new Point(1275, 623);
+            btCheckL.Name = "btCheckL";
+            btCheckL.Size = new Size(249, 76);
+            btCheckL.TabIndex = 6;
+            btCheckL.Text = "Kiểm tra tình trạng";
+            btCheckL.UseVisualStyleBackColor = true;
+            // 
+            // btSetL
+            // 
+            btSetL.Dock = DockStyle.Bottom;
+            btSetL.Font = new Font("Segoe UI", 9.857143F);
+            btSetL.Location = new Point(1275, 517);
+            btSetL.Name = "btSetL";
+            btSetL.Size = new Size(249, 76);
+            btSetL.TabIndex = 5;
+            btSetL.Text = "Tính toán lương";
+            btSetL.UseVisualStyleBackColor = true;
+            // 
+            // LuongGV
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1548, 821);
+            Controls.Add(tableLayoutPanel1);
+            Name = "LuongGV";
+            Text = "LuongGV";
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label LaL;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TextBox tbTL;
+        private Label laMGV;
+        private Label laTGV;
+        private TextBox tbML;
+        private ListView listView1;
+        private Button btT;
+        private Button btCheckL;
+        private Button btSetL;
+    }
+}
---
+++ AdminTC/LuongGV.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PBL3a.UI.AdminTC
+{
+    public partial class LuongGV : Form
+    {
+        public LuongGV()
+        {
+            InitializeComponent();
+        }
+    }
+}
