7b972f57609890985b56e8b012bb23b14d8e772f
Le Xuan Nam <lnamdeptrai392@gmail.com>
2026-03-08 22:38:54 +0700

    1
    1

--- PBL3a/data/Accountlist.sql
+++ PBL3a/data/Accountlist.sql
@@ -1,3 +1,18 @@
-USE PBL3aDB
+CREATE DATABASE PBL3aDB;
+GO
 
-SELECT * FROM accountList
+USE PBL3aDB;
+GO
+
+CREATE TABLE accountList (
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(100) NOT NULL,
+    Role NVARCHAR(20) NOT NULL
+);
+
+INSERT INTO accountList (Username, Password, Role)
+VALUES
+('adminc', '123', 'AdminC'),
+('admindd', '123', 'AdminDD'),
+('admintc', '123', 'AdminTC');
