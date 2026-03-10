5149cef2b2238c8cca89385f471bb2bede3801dd
Nguyễn Phương Uyên <dwilrag@gmail.com>
2026-03-10 22:49:56 +0700

    Change Student
    hi

--- PBL3a/UI/Student/StudentAll.Designer.cs
+++ PBL3a/UI/Student/StudentAll.Designer.cs
@@ -28,60 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
+            btn_fee = new Button();
+            btn_schedule = new Button();
+            btn_info = new Button();
+            btn_score = new Button();
             tableLayoutPanel1 = new TableLayoutPanel();
-            button6 = new Button();
-            button2 = new Button();
+            btn_exit = new Button();
+            btn_material = new Button();
             tableLayoutPanel1.SuspendLayout();
             SuspendLayout();
             // 
-            // button1
+            // btn_fee
             // 
-            button1.Anchor = AnchorStyles.Right;
-            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button1.Location = new Point(42, 277);
-            button1.Name = "button1";
-            button1.Size = new Size(206, 120);
-            button1.TabIndex = 4;
-            button1.Text = "Học phí";
-            button1.UseVisualStyleBackColor = true;
+            btn_fee.Anchor = AnchorStyles.Right;
+            btn_fee.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_fee.Location = new Point(42, 277);
+            btn_fee.Name = "btn_fee";
+            btn_fee.Size = new Size(206, 120);
+            btn_fee.TabIndex = 4;
+            btn_fee.Text = "Học phí";
+            btn_fee.UseVisualStyleBackColor = true;
+            btn_fee.Click += btn_fee_Click;
             // 
-            // button3
+            // btn_schedule
             // 
-            button3.Anchor = AnchorStyles.Left;
-            button3.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button3.Location = new Point(544, 52);
-            button3.Name = "button3";
-            button3.Size = new Size(206, 120);
-            button3.TabIndex = 6;
-            button3.Text = "Thời khóa biểu";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
+            btn_schedule.Anchor = AnchorStyles.Left;
+            btn_schedule.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_schedule.Location = new Point(544, 52);
+            btn_schedule.Name = "btn_schedule";
+            btn_schedule.Size = new Size(206, 120);
+            btn_schedule.TabIndex = 6;
+            btn_schedule.Text = "Thời khóa biểu";
+            btn_schedule.UseVisualStyleBackColor = true;
+            btn_schedule.Click += btn_schedule_Click;
             // 
-            // button4
+            // btn_info
             // 
-            button4.Anchor = AnchorStyles.Right;
-            button4.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button4.Location = new Point(42, 52);
-            button4.Name = "button4";
-            button4.Size = new Size(206, 120);
-            button4.TabIndex = 7;
-            button4.Text = "Thông tin cá nhân";
-            button4.UseVisualStyleBackColor = true;
+            btn_info.Anchor = AnchorStyles.Right;
+            btn_info.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_info.Location = new Point(42, 52);
+            btn_info.Name = "btn_info";
+            btn_info.Size = new Size(206, 120);
+            btn_info.TabIndex = 7;
+            btn_info.Text = "Thông tin cá nhân";
+            btn_info.UseVisualStyleBackColor = true;
+            btn_info.Click += btn_info_Click;
             // 
-            // button5
+            // btn_score
             // 
-            button5.Anchor = AnchorStyles.None;
-            button5.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button5.Location = new Point(293, 52);
-            button5.Name = "button5";
-            button5.Size = new Size(206, 120);
-            button5.TabIndex = 8;
-            button5.Text = "Kết quả học tập";
-            button5.UseVisualStyleBackColor = true;
+            btn_score.Anchor = AnchorStyles.None;
+            btn_score.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_score.Location = new Point(293, 52);
+            btn_score.Name = "btn_score";
+            btn_score.Size = new Size(206, 120);
+            btn_score.TabIndex = 8;
+            btn_score.Text = "Kết quả học tập";
+            btn_score.UseVisualStyleBackColor = true;
+            btn_score.Click += btn_score_Click;
             // 
             // tableLayoutPanel1
             // 
@@ -89,12 +92,12 @@
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 46.4455F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 53.5545F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 258F));
-            tableLayoutPanel1.Controls.Add(button6, 2, 1);
-            tableLayoutPanel1.Controls.Add(button2, 1, 1);
-            tableLayoutPanel1.Controls.Add(button5, 1, 0);
-            tableLayoutPanel1.Controls.Add(button3, 2, 0);
-            tableLayoutPanel1.Controls.Add(button4, 0, 0);
-            tableLayoutPanel1.Controls.Add(button1, 0, 1);
+            tableLayoutPanel1.Controls.Add(btn_exit, 2, 1);
+            tableLayoutPanel1.Controls.Add(btn_material, 1, 1);
+            tableLayoutPanel1.Controls.Add(btn_score, 1, 0);
+            tableLayoutPanel1.Controls.Add(btn_schedule, 2, 0);
+            tableLayoutPanel1.Controls.Add(btn_info, 0, 0);
+            tableLayoutPanel1.Controls.Add(btn_fee, 0, 1);
             tableLayoutPanel1.Dock = DockStyle.Fill;
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
@@ -105,27 +108,29 @@
             tableLayoutPanel1.Size = new Size(800, 450);
             tableLayoutPanel1.TabIndex = 10;
             // 
-            // button6
+            // btn_exit
             // 
-            button6.Anchor = AnchorStyles.Left;
-            button6.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button6.Location = new Point(544, 277);
-            button6.Name = "button6";
-            button6.Size = new Size(206, 120);
-            button6.TabIndex = 11;
-            button6.Text = "Đăng xuất";
-            button6.UseVisualStyleBackColor = true;
+            btn_exit.Anchor = AnchorStyles.Left;
+            btn_exit.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_exit.Location = new Point(544, 277);
+            btn_exit.Name = "btn_exit";
+            btn_exit.Size = new Size(206, 120);
+            btn_exit.TabIndex = 11;
+            btn_exit.Text = "Đăng xuất";
+            btn_exit.UseVisualStyleBackColor = true;
+            btn_exit.Click += btn_exit_Click;
             // 
-            // button2
+            // btn_material
             // 
-            button2.Anchor = AnchorStyles.None;
-            button2.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
-            button2.Location = new Point(293, 277);
-            button2.Name = "button2";
-            button2.Size = new Size(206, 120);
-            button2.TabIndex = 10;
-            button2.Text = "Tài liệu";
-            button2.UseVisualStyleBackColor = true;
+            btn_material.Anchor = AnchorStyles.None;
+            btn_material.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 163);
+            btn_material.Location = new Point(293, 277);
+            btn_material.Name = "btn_material";
+            btn_material.Size = new Size(206, 120);
+            btn_material.TabIndex = 10;
+            btn_material.Text = "Tài liệu";
+            btn_material.UseVisualStyleBackColor = true;
+            btn_material.Click += btn_material_Click;
             // 
             // StudentAll
             // 
@@ -141,12 +146,12 @@
         }
 
         #endregion
-        private Button button1;
-        private Button button3;
-        private Button button4;
-        private Button button5;
+        private Button btn_fee;
+        private Button btn_schedule;
+        private Button btn_info;
+        private Button btn_score;
         private TableLayoutPanel tableLayoutPanel1;
-        private Button button6;
-        private Button button2;
+        private Button btn_exit;
+        private Button btn_material;
     }
 }
--- PBL3a/UI/Student/StudentAll.cs
+++ PBL3a/UI/Student/StudentAll.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Windows.Forms;
+﻿using PBL3a.UI.Login;
 
 namespace PBL3a.UI.Student
 {
@@ -14,20 +8,59 @@ namespace PBL3a.UI.Student
         {
             InitializeComponent();
         }
-// //
+        // //
         private void StudentAll_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void btn_schedule_Click(object sender, EventArgs e)
         {
-
+            StudentSchedule schedule = new StudentSchedule();
+            this.Hide();
+            schedule.ShowDialog();
+            this.Show();
         }
 
-        private void button6_Click(object sender, EventArgs e)
-        {
+  
 
+        private void btn_info_Click(object sender, EventArgs e)
+        {
+            StudentINFO info = new StudentINFO();
+            this.Hide();
+            info.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_score_Click(object sender, EventArgs e)
+        {
+            StudentScore score = new StudentScore();
+            this.Hide();
+            score.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_fee_Click(object sender, EventArgs e)
+        {
+            StudentFee fee = new StudentFee();
+            this.Hide();
+            fee.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_material_Click(object sender, EventArgs e)
+        {
+            StudentMaterial material = new StudentMaterial();
+            this.Hide();
+            material.ShowDialog();
+            this.Show();
+        }
+
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            LoginForm f = new LoginForm();
+            f.Show();
+            this.Close();
         }
     }
 }
