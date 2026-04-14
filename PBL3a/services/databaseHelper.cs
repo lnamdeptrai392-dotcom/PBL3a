0ed6354cbe259d4488f10cfaca646428b1ed5f66
Le Xuan Nam <lnamdeptrai392@gmail.com>
2026-04-14 20:34:30 +0700

    Revert "Delete PBL3a/services/databaseHelper.cs"
    This reverts commit c9d4a6c04379c5d158701e5ef1b3987cbaa6dfad.

---
+++ PBL3a/services/databaseHelper.cs
@@ -0,0 +1,16 @@
+using Microsoft.Data.SqlClient;
+
+namespace PBL3a.services
+{
+    public class DatabaseHelper
+    {
+        //thay the sau server=...; thay thanh server cua ban
+        private string connectionString =
+        "Server=.\\SQLEXPRESS;Database=PBL3aDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(connectionString);
+        }
+    }
+}
