8feaa9bb31122fce2b27fd12e0862280b2ed7437
Phạm Thị Ngọc Khuê <ptnkhuee@gmail.com>
2026-04-21 22:54:50 +0700

    Fix UI and logic
    I add Time to solve Teacher's salary
    - you can only see the information of time in the textbook.  if they don't have value we can see all salary of teacher.
    - i also fix the synchronous salary information, you also can fix 'thuong', 'phat' or 'ghi chu' on the datagridview

--- PBL3a/UI/AdminTC/LuongGV.Designer.cs
+++ PBL3a/UI/AdminTC/LuongGV.Designer.cs
@@ -24,6 +24,10 @@ namespace PBL3a.UI.AdminTC
             dataGridView1 = new DataGridView();
             cbbMGV = new ComboBox();
             btSetL = new Button();
+            laThang = new Label();
+            laNam = new Label();
+            cbbThang = new ComboBox();
+            txtNam = new TextBox();
             tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
@@ -32,44 +36,49 @@ namespace PBL3a.UI.AdminTC
             // 
             tableLayoutPanel1.BackColor = Color.AliceBlue;
             tableLayoutPanel1.ColumnCount = 8;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 21F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 12F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 32.5F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 67.5F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 182F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 384F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 15F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 213F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 22F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 106F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 224F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 9F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 124F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 13F));
             tableLayoutPanel1.Controls.Add(tbTL, 4, 1);
             tableLayoutPanel1.Controls.Add(laMGV, 1, 1);
             tableLayoutPanel1.Controls.Add(laTGV, 3, 1);
-            tableLayoutPanel1.Controls.Add(dataGridView1, 1, 3);
+            tableLayoutPanel1.Controls.Add(dataGridView1, 1, 4);
             tableLayoutPanel1.Controls.Add(cbbMGV, 2, 1);
-            tableLayoutPanel1.Controls.Add(btSetL, 6, 6);
+            tableLayoutPanel1.Controls.Add(btSetL, 6, 7);
+            tableLayoutPanel1.Controls.Add(laThang, 1, 2);
+            tableLayoutPanel1.Controls.Add(laNam, 3, 2);
+            tableLayoutPanel1.Controls.Add(cbbThang, 2, 2);
+            tableLayoutPanel1.Controls.Add(txtNam, 4, 2);
             tableLayoutPanel1.Font = new Font("Microsoft Sans Serif", 9F);
-            tableLayoutPanel1.Location = new Point(2, 2);
-            tableLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
+            tableLayoutPanel1.Location = new Point(1, 1);
+            tableLayoutPanel1.Margin = new Padding(2);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
-            tableLayoutPanel1.RowCount = 8;
+            tableLayoutPanel1.RowCount = 9;
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 32.83582F));
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 67.16418F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 16F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 428F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 68F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 62F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 94F));
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 142F));
-            tableLayoutPanel1.Size = new Size(1478, 940);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 20F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 19F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 203F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 34F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 31F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 47F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Absolute, 71F));
+            tableLayoutPanel1.Size = new Size(862, 470);
             tableLayoutPanel1.TabIndex = 1;
             // 
             // tbTL
             // 
             tbTL.Dock = DockStyle.Fill;
             tbTL.Enabled = false;
-            tbTL.Location = new Point(846, 46);
-            tbTL.Margin = new Padding(3, 4, 3, 4);
+            tbTL.Location = new Point(493, 16);
+            tbTL.Margin = new Padding(2);
             tbTL.Name = "tbTL";
-            tbTL.Size = new Size(378, 31);
+            tbTL.Size = new Size(220, 21);
             tbTL.TabIndex = 4;
             // 
             // laMGV
@@ -78,9 +87,10 @@ namespace PBL3a.UI.AdminTC
             laMGV.Dock = DockStyle.Right;
             laMGV.Font = new Font("Microsoft Sans Serif", 9F);
             laMGV.ForeColor = Color.Black;
-            laMGV.Location = new Point(60, 42);
+            laMGV.Location = new Point(37, 14);
+            laMGV.Margin = new Padding(2, 0, 2, 0);
             laMGV.Name = "laMGV";
-            laMGV.Size = new Size(166, 87);
+            laMGV.Size = new Size(94, 30);
             laMGV.TabIndex = 1;
             laMGV.Text = "Mã Giảng Viên: ";
             // 
@@ -90,9 +100,10 @@ namespace PBL3a.UI.AdminTC
             laTGV.Dock = DockStyle.Right;
             laTGV.Font = new Font("Microsoft Sans Serif", 9F);
             laTGV.ForeColor = Color.Black;
-            laTGV.Location = new Point(667, 42);
+            laTGV.Location = new Point(392, 14);
+            laTGV.Margin = new Padding(2, 0, 2, 0);
             laTGV.Name = "laTGV";
-            laTGV.Size = new Size(173, 87);
+            laTGV.Size = new Size(97, 30);
             laTGV.TabIndex = 2;
             laTGV.Text = "Tên Giảng Viên: ";
             // 
@@ -119,22 +130,23 @@ namespace PBL3a.UI.AdminTC
             dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             dataGridView1.Dock = DockStyle.Fill;
             dataGridView1.GridColor = Color.Gray;
-            dataGridView1.Location = new Point(24, 149);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Location = new Point(14, 85);
+            dataGridView1.Margin = new Padding(2);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
             tableLayoutPanel1.SetRowSpan(dataGridView1, 4);
-            dataGridView1.Size = new Size(1200, 644);
+            dataGridView1.Size = new Size(699, 311);
             dataGridView1.TabIndex = 8;
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
             // 
             // cbbMGV
             // 
             cbbMGV.Dock = DockStyle.Fill;
             cbbMGV.FormattingEnabled = true;
-            cbbMGV.Location = new Point(232, 46);
-            cbbMGV.Margin = new Padding(3, 4, 3, 4);
+            cbbMGV.Location = new Point(135, 16);
+            cbbMGV.Margin = new Padding(2);
             cbbMGV.Name = "cbbMGV";
-            cbbMGV.Size = new Size(426, 33);
+            cbbMGV.Size = new Size(248, 23);
             cbbMGV.TabIndex = 9;
             cbbMGV.TextUpdate += comboBox1_TextUpdate;
             // 
@@ -143,10 +155,10 @@ namespace PBL3a.UI.AdminTC
             btSetL.BackColor = Color.FromArgb(112, 146, 190);
             btSetL.Font = new Font("Segoe UI", 9.857143F);
             btSetL.ForeColor = Color.White;
-            btSetL.Location = new Point(1245, 707);
-            btSetL.Margin = new Padding(3, 4, 3, 4);
+            btSetL.Location = new Point(726, 353);
+            btSetL.Margin = new Padding(2);
             btSetL.Name = "btSetL";
-            btSetL.Size = new Size(204, 76);
+            btSetL.Size = new Size(119, 38);
             btSetL.TabIndex = 5;
             btSetL.Text = "Tính toán lương";
             btSetL.UseVisualStyleBackColor = false;
@@ -154,15 +166,58 @@ namespace PBL3a.UI.AdminTC
             btSetL.MouseEnter += btSetL_MouseEnter;
             btSetL.MouseLeave += btSetL_MouseLeave;
             // 
+            // laThang
+            // 
+            laThang.AutoSize = true;
+            laThang.Dock = DockStyle.Right;
+            laThang.Font = new Font("Microsoft Sans Serif", 9F);
+            laThang.ForeColor = Color.Black;
+            laThang.Location = new Point(86, 44);
+            laThang.Margin = new Padding(2, 0, 2, 0);
+            laThang.Name = "laThang";
+            laThang.Size = new Size(45, 20);
+            laThang.TabIndex = 10;
+            laThang.Text = "Tháng:";
+            // 
+            // laNam
+            // 
+            laNam.AutoSize = true;
+            laNam.Dock = DockStyle.Right;
+            laNam.Font = new Font("Microsoft Sans Serif", 9F);
+            laNam.ForeColor = Color.Black;
+            laNam.Location = new Point(452, 44);
+            laNam.Margin = new Padding(2, 0, 2, 0);
+            laNam.Name = "laNam";
+            laNam.Size = new Size(37, 20);
+            laNam.TabIndex = 11;
+            laNam.Text = "Năm:";
+            // 
+            // cbbThang
+            // 
+            cbbThang.AutoCompleteCustomSource.AddRange(new string[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" });
+            cbbThang.FormattingEnabled = true;
+            cbbThang.Items.AddRange(new object[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" });
+            cbbThang.Location = new Point(136, 47);
+            cbbThang.Name = "cbbThang";
+            cbbThang.Size = new Size(246, 23);
+            cbbThang.TabIndex = 12;
+            // 
+            // txtNam
+            // 
+            txtNam.Location = new Point(494, 47);
+            txtNam.Name = "txtNam";
+            txtNam.Size = new Size(218, 21);
+            txtNam.TabIndex = 13;
+            // 
             // LuongGV
             // 
-            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
-            ClientSize = new Size(1481, 824);
+            ClientSize = new Size(864, 412);
             Controls.Add(tableLayoutPanel1);
             FormBorderStyle = FormBorderStyle.None;
-            Margin = new Padding(3, 4, 3, 4);
+            Margin = new Padding(2);
             Name = "LuongGV";
             Text = "LuongGV";
             tableLayoutPanel1.ResumeLayout(false);
@@ -177,5 +232,9 @@ namespace PBL3a.UI.AdminTC
         private DataGridView dataGridView1;
         private Button btSetL;
         private ComboBox cbbMGV;
+        private Label laThang;
+        private Label laNam;
+        private ComboBox cbbThang;
+        private TextBox txtNam;
     }
 }
--- PBL3a/UI/AdminTC/LuongGV.cs
+++ PBL3a/UI/AdminTC/LuongGV.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
+using PBL3;
 using PBL3a.services;
 using System;
 using System.Data;
@@ -151,6 +152,44 @@ namespace PBL3a.UI.AdminTC
             LoadLuongTheoGV(teacherID);
         }
 
+        private void LoadLuongTheoThang(int month, int year, string teacherID)
+        {
+            using (SqlConnection conn = db.GetConnection())
+            {
+                conn.Open();
+
+                string query = @"
+                    SELECT 
+                        LuongID,
+                        TeacherID AS [Mã GV],
+                        SalaryMonth AS [Tháng],
+                        SalaryYear AS [Năm],
+                        SoLopDay AS [Số lớp dạy],
+                        SoBuoiDay AS [Số buổi dạy],
+                        LuongCoBan AS [Lương cơ bản],
+                        Thuong AS [Thưởng],
+                        Phat AS [Phạt],
+                        TongLuong AS [Tổng lương],
+                        TrangThai AS [Trạng thái],
+                        NgayThanhToan AS [Ngày thanh toán],
+                        GhiChu AS [Ghi chú]
+                    FROM LuongGV
+                    WHERE TeacherID = @teacherID
+                    AND SalaryMonth = @month 
+                    AND SalaryYear = @year";
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(query, conn))
+                {
+                    adapter.SelectCommand.Parameters.AddWithValue("@teacherID", teacherID);
+                    adapter.SelectCommand.Parameters.AddWithValue("@month", month);
+                    adapter.SelectCommand.Parameters.AddWithValue("@year", year);
+                    dtLuong = new DataTable();
+                    adapter.Fill(dtLuong);
+                    dataGridView1.DataSource = dtLuong;
+                }
+            }
+        }
+
         private void btSetL_Click(object sender, EventArgs e)
         {
             if (cbbMGV.SelectedItem == null)
@@ -160,17 +199,30 @@ namespace PBL3a.UI.AdminTC
             }
 
             string teacherID = cbbMGV.SelectedItem.ToString();
+            LoadLuongTheoGV(teacherID);
+
             int month = DateTime.Now.Month;
             int year = DateTime.Now.Year;
+
+            if (cbbThang.SelectedItem != null && txtNam.Text != "")
+            {
+                month = Convert.ToInt32(cbbThang.SelectedItem.ToString());
+                year = Convert.ToInt32(txtNam.Text);
+            }
+            else if ((cbbThang.SelectedItem != null || txtNam.Text == "") && (cbbThang.SelectedItem == null || txtNam.Text != ""))
+            {
+                MessageBox.Show("Hay chon thang va nam");
+                return;
+            }
 
             using (SqlConnection conn = db.GetConnection())
             {
                 conn.Open();
 
                 string checkQuery = @"
-                    SELECT COUNT(*)
-                    FROM LuongGV
-                    WHERE TeacherID = @teacherID AND SalaryMonth = @month AND SalaryYear = @year";
+                SELECT COUNT(*)
+                FROM LuongGV
+                WHERE TeacherID = @teacherID AND SalaryMonth = @month AND SalaryYear = @year";
 
                 using (SqlCommand checkCmd = new SqlCommand(checkQuery, conn))
                 {
@@ -183,16 +235,21 @@ namespace PBL3a.UI.AdminTC
                     if (exists > 0)
                     {
                         MessageBox.Show("Tháng này đã có bảng lương cho giảng viên.");
+                        LoadLuongTheoThang(month, year, teacherID);
                         return;
                     }
                 }
 
-                string classCountQuery = "SELECT COUNT(*) FROM Class WHERE teacherID = @teacherID";
+                string classCountQuery = @"SELECT COUNT(*) FROM Class WHERE teacherID = @teacherID
+                                        AND start_date <= EOMONTH(DATEFROMPARTS(@year, @month, 1))
+                                        AND end_date >= DATEFROMPARTS(@year, @month, 1)";
                 int soLopDay = 0;
 
                 using (SqlCommand cmd = new SqlCommand(classCountQuery, conn))
                 {
                     cmd.Parameters.AddWithValue("@teacherID", teacherID);
+                    cmd.Parameters.AddWithValue("@month", month);
+                    cmd.Parameters.AddWithValue("@year", year);
                     soLopDay = (int)cmd.ExecuteScalar();
                 }
 
@@ -203,10 +260,10 @@ namespace PBL3a.UI.AdminTC
                 decimal tongLuong = luongCoBan + thuong - phat;
 
                 string insertQuery = @"
-                    INSERT INTO LuongGV
-                    (TeacherID, SalaryMonth, SalaryYear, SoLopDay, SoBuoiDay, LuongCoBan, Thuong, Phat, TongLuong, TrangThai, NgayThanhToan, GhiChu)
-                    VALUES
-                    (@teacherID, @month, @year, @soLopDay, @soBuoiDay, @luongCoBan, @thuong, @phat, @tongLuong, N'Chưa thanh toán', NULL, NULL)";
+                INSERT INTO LuongGV
+                (TeacherID, SalaryMonth, SalaryYear, SoLopDay, SoBuoiDay, LuongCoBan, Thuong, Phat, TongLuong, TrangThai, NgayThanhToan, GhiChu)
+                VALUES
+                (@teacherID, @month, @year, @soLopDay, @soBuoiDay, @luongCoBan, @thuong, @phat, @tongLuong, N'Chưa thanh toán', NULL, NULL)";
 
                 using (SqlCommand insertCmd = new SqlCommand(insertQuery, conn))
                 {
@@ -259,6 +316,58 @@ namespace PBL3a.UI.AdminTC
             else
             {
                 cbbMGV.DroppedDown = false;
+            }
+        }
+
+        private void CapNhatThuongPhatXuongDB(int luongID, decimal thuong, decimal phat, decimal tongLuong)
+        {
+            using (SqlConnection conn = db.GetConnection())
+            {
+                string query = @"
+                                UPDATE LuongGV 
+                                SET Thuong = @thuong, 
+                                    Phat = @phat, 
+                                    TongLuong = @tongLuong 
+                                WHERE LuongID = @id";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@thuong", thuong);
+                    cmd.Parameters.AddWithValue("@phat", phat);
+                    cmd.Parameters.AddWithValue("@tongLuong", tongLuong);
+                    cmd.Parameters.AddWithValue("@id", luongID);
+
+                    try
+                    {
+                        conn.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Lỗi đồng bộ CSDL: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0) return;
+            string colName = dataGridView1.Columns[e.ColumnIndex].Name;
+            if (colName == "Thưởng" || colName == "Phạt")
+            {
+                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
+
+                decimal luongCB = Convert.ToDecimal(row.Cells["Lương cơ bản"].Value != DBNull.Value ? row.Cells["Lương cơ bản"].Value : 0);
+                decimal thuong = Convert.ToDecimal(row.Cells["Thưởng"].Value != DBNull.Value ? row.Cells["Thưởng"].Value : 0);
+                decimal phat = Convert.ToDecimal(row.Cells["Phạt"].Value != DBNull.Value ? row.Cells["Phạt"].Value : 0);
+
+                decimal tongLuong = luongCB + thuong - phat;
+
+                row.Cells["Tổng lương"].Value = tongLuong;
+
+                int luongID = Convert.ToInt32(row.Cells["LuongID"].Value);
+                CapNhatThuongPhatXuongDB(luongID, thuong, phat, tongLuong);
             }
         }
     }
